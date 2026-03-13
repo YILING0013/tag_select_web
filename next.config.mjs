174b11fa5264b3b4fbfb2a9f37af35495d7c892e
IdleCloud <115537554+YILING0013@users.noreply.github.com>
2026-03-13 14:23:52 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    //打包时取消注释
-    basePath: '/static/frontend',  // 设置基础路径
-    assetPrefix: '/static/frontend',  // 设置静态资源路径前缀
+    // output: 'export',
+    // //打包时取消注释
+    // basePath: '/static/frontend',  // 设置基础路径
+    // assetPrefix: '/static/frontend',  // 设置静态资源路径前缀
   };
   
   export default nextConfig;
